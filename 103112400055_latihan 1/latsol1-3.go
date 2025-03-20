483de9c4d8cfc5b756c7401ebcfd318435ca4590
Feros999 <ferospedrosa@gmail.com>
2025-03-20 07:53:50 +0700

    Add files via upload

---
+++ 103112400055_latihan 1/latsol1-3.go
@@ -0,0 +1,5 @@
+fmt.Println("\nContoh for dengan range:")
+	buah := []string{"Apel", "Mangga", "Jeruk", "Pisang"}
+	for ________, item := range buah { // (5) Lengkapi bagian ini agar mencetak indeks dan nama buah
+		_________________________ // (6) Lengkapi bagian ini untuk mencetak "Buah pada index X adalah Y"
+	}
